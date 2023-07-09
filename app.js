1fac5e957a54f953d7e81f6ca377ebf3920a10f0
utsav000 <utsavgupta8770@gmail.com>
2023-07-09 23:43:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookiePaser = require("cookie-parser");
+
+const Blog = require("./models/blog");
+
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then((e) => console.log("MongoDB Connected"));
+// const PORT = process.env.PORT || 8000;
+
+const dbURL =  'mongodb://127.0.0.1:27017/blog';
+mongoose.connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
